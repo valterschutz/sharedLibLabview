b980b10cdae51afee493203d7d916de284a6a2f2
Valter Schutz <valterschutz@proton.me>
2023-11-23 11:12:02 +0100

    x86 cross-compilation, does not yet work

---
+++ Toolchain_x86.cmake
@@ -0,0 +1,4 @@
+set(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_SYSTEM_PROCESSOR i686)
+set(CMAKE_C_COMPILER "C:/Program Files/mingw32/bin/gcc.exe")
+set(CMAKE_CXX_COMPILER "C:/Program Files/mingw32/bin/g++.exe")
